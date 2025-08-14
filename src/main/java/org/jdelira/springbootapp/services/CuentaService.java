f6eedbb540cfe318b2b46dc278ea8038535c00ec
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-08-14 13:54:00 -0700

    Test con Junit y Mockito

--- src/main/java/org/jdelira/springbootapp/services/CuentaService.java
+++ src/main/java/org/jdelira/springbootapp/services/CuentaService.java
@@ -5,11 +5,13 @@ import org.jdelira.springbootapp.models.Cuenta;
 import java.math.BigDecimal;
 
 public interface CuentaService {
+
     Cuenta findById(Long id);
 
     int revisarTotalTransferencias(Long bancoId);
 
     BigDecimal revisarSaldo(Long cuentaId);
 
-    void transferir(Long numCuentaOrigen, Long numCuentaDestino, BigDecimal monto);
+    void transferir(Long numCuentaOrigen, Long numCuentaDestino, BigDecimal monto,
+                    Long bancoId);
 }
--- src/test/java/org/jdelira/springbootapp/SpringbootTestApplicationTests.java
+++ src/test/java/org/jdelira/springbootapp/SpringbootTestApplicationTests.java
@@ -1,13 +1,53 @@
 package org.jdelira.springbootapp;
 
+import org.jdelira.springbootapp.DAO.BancoRepository;
+import org.jdelira.springbootapp.DAO.CuentaRepository;
+import org.jdelira.springbootapp.services.CuentaService;
+import org.jdelira.springbootapp.services.CuentaServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class SpringbootTestApplicationTests {
 
+	CuentaRepository cuentaRepository;
+	BancoRepository bancoRepository;
+
+	CuentaService service;
+
+	@BeforeEach
+	void setUp() {
+
+		cuentaRepository = mock(CuentaRepository.class);
+		bancoRepository = mock(BancoRepository.class);
+		service = new CuentaServiceImpl(cuentaRepository, bancoRepository);
+	}
+
 	@Test
 	void contextLoads() {
+		when(cuentaRepository.findById(1L)).thenReturn(Datos.CUENTA_001);
+		when(cuentaRepository.findById(2L)).thenReturn(Datos.CUENTA_002);
+		when(bancoRepository.findById(1L)).thenReturn(Datos.BANCO);
+
+		BigDecimal saldoOrigen = service.revisarSaldo(1L);
+		BigDecimal saldoDestino = service.revisarSaldo(2L);
+		assertEquals("1000", saldoOrigen.toPlainString());
+		assertEquals("2000", saldoDestino.toPlainString());
+
+		service.transferir(1L, 2L, new BigDecimal("100"), 1l);
+
+		saldoOrigen = service.revisarSaldo(1L);
+		saldoDestino = service.revisarSaldo(2L);
+
+		assertEquals("900", saldoOrigen.toPlainString());
+		assertEquals("2100", saldoDestino.toPlainString());
 	}
 
 }
