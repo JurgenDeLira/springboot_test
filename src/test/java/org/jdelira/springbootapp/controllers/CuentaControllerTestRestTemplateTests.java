74f8e8f62b7adc89aaf496bf8ce4a6c837e895ee
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-08-21 12:05:53 -0700

    Configuración y pruebas integración TestRestTemplate

---
+++ src/test/java/org/jdelira/springbootapp/controllers/CuentaControllerTestRestTemplateTests.java
@@ -0,0 +1,60 @@
+package org.jdelira.springbootapp.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jdelira.springbootapp.models.TransaccionDto;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CuentaControllerTestRestTemplateTests {
+
+    @Autowired
+    private TestRestTemplate client;
+
+    private ObjectMapper objectMapper;
+
+    @LocalServerPort
+    private int puerto;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    @Order(1)
+    void testTransferir() {
+        TransaccionDto dto = new TransaccionDto();
+        dto.setMonto(new BigDecimal("100"));
+        dto.setCuentaDestinoId(2L);
+        dto.setCuentaOrigenId(1L);
+        dto.setBancoId(1L);
+
+        ResponseEntity<String> response = client
+                .postForEntity(crearUri( "/api/cuentas/transferir"), dto, String.class);
+        System.out.println(puerto);
+        String json = response.getBody();
+        System.out.println(json);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
+        assertNotNull(json);// o puede ser: assert json != null;
+        assertTrue(json.contains("Transferencia realizada con éxito!"));
+        assertTrue(json.contains("{\"cuentaOrigenId\":1,\"cuentaDestinoId\":2,\"monto\":100,\"bancoId\":1}"));
+
+    }
+
+    private String crearUri(String uri) {
+        return "http://localhost:" + puerto + uri;
+    }
+}
